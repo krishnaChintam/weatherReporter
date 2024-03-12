101479aea5dd1aba535ed7fd63a484d9574be003
krishnaChintam <163150396+krishnaChintam@users.noreply.github.com>
2024-03-13 01:30:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import WeatherTable from './components/weatherCard/WeatherCard';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div className="App">
+    <ToastContainer />
       <WeatherTable />
     </div>
   );
